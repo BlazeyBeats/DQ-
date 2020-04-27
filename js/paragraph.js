98c2df632ff7907e0aac67a20a18020ff24bf1e5
Blazey Beats <yoyomiao1999@gmail.com>
2020-04-27 21:27:23 +0800

    dropout fixed!!!!!

--- js/paragraph.js
+++ js/paragraph.js
@@ -9,15 +9,17 @@ $(document).ready(function(){
 
 
 function openNav() {
-    document.getElementById("mySidenav-fixed").style.visibility = "hidden";
+    document.getElementById("mySidenav-fixed").style.display = "none";
     document.getElementById("mySidenav").style.width = "230px";
+    document.getElementById("mySidenav").style.display = "block";
     document.getElementById("main").style.marginLeft = "230px";
     
    }
 
   function closeNav() {
-    document.getElementById("mySidenav-fixed").style.visibility = "visible";
+    document.getElementById("mySidenav-fixed").style.display = "block";
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.display = "none";
     document.getElementById("main").style.marginLeft = "0";
     
   }
