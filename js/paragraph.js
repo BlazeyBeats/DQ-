5ce142b95014c1dbe4d17c792d06d274e9f6ff59
Blazey Beats <yoyomiao1999@gmail.com>
2020-04-27 19:43:19 +0800

    main wip

--- js/paragraph.js
+++ js/paragraph.js
@@ -12,13 +12,14 @@ function openNav() {
     document.getElementById("mySidenav-fixed").style.visibility = "hidden";
     document.getElementById("mySidenav").style.width = "230px";
     document.getElementById("main").style.marginLeft = "230px";
-    
+    document.getElementById("main").style.paddingLeft = "50px";
    }
 
   function closeNav() {
     document.getElementById("mySidenav-fixed").style.visibility = "visible";
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("main").style.paddingLeft = "100px";
     
   }
 
